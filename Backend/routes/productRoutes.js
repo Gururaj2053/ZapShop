ddb79ca3e4805ef27e837563bbf13532c7b7d471
Gururaj2053 <148246805+Gururaj2053@users.noreply.github.com>
2024-12-28 22:03:56 +0530

    Bug Fixes

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -8,7 +8,7 @@ import {
   deleteProduct,
   createProductReview,
   getTopProducts,
-} from '../Controllers/productController.js';
+} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 import checkObjectId from '../middleware/checkObjectId.js';
 
